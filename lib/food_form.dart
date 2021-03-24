283f8773647bdf6d2dbe9cc8e268093308b286ac
AadityaCholayil <cholayilaamur@student.mes.ac.in>
2021-03-24 18:07:21 +0530

    food form ui padding

--- lib/food_form.dart
+++ lib/food_form.dart
@@ -17,8 +17,14 @@ class _FoodFormState extends State<FoodForm> {
   bool isSearching=false;
   bool searchDone=false;
   FoodList foodList;
+  double screenHeight, screenWidth;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   Future<dynamic> getData() async {
     Response response = await get("https://nutritionix-api.p.rapidapi.com/v1_1/search/$foodName?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein",
@@ -55,18 +61,74 @@ class _FoodFormState extends State<FoodForm> {
     );
   }
 
+  Widget customFoodButton(){
+    return TextButton(
+      child: Text(
+        'Create a Custom One.',
+        style: TextStyle(
+          fontSize: 20,
+          color: Theme.of(context).accentColor,
+          decoration: TextDecoration.underline,
+        ),
+      ),
+      onPressed: (){
+        Food food;
+        food = Food(
+          name: foodName,
+          calories: 0,
+          fats: 0,
+          protein: 0,
+          carbohydrates: 0,
+          servingSizeQty: 1,
+          servingSizeUnit: 'serving',
+        );
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (BuildContext context) => FoodFormFinal(food: food,))
+        );
+      },
+    );
+  }
+
   Widget _buildList(FoodList foodList){
     if(foodList.list.length==0){
-      return Text('No results found');
+      return Column(
+        children: [
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'No results found.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              customFoodButton(),
+            ],
+          ),
+          SizedBox(height: 18),
+        ],
+      );
     }
     return Container(
       alignment: Alignment.topCenter,
       child: Column(
         children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(30, 5, 0, 0),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Select One:',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          //Divider(thickness: 2),
           ListView.separated(
             padding: EdgeInsets.zero,
             scrollDirection: Axis.vertical,
-            //primary: false,
             shrinkWrap: true,
             itemBuilder: (BuildContext context, int index){
               Food food=foodList.list[index];
@@ -112,19 +174,56 @@ class _FoodFormState extends State<FoodForm> {
             separatorBuilder: (BuildContext context, int index) => Divider(thickness: 2),
           ),
           Divider(thickness: 2),
+          SizedBox(height: 4,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Not in the List?',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              customFoodButton(),
+            ],
+          ),
+          SizedBox(height: 6,)
         ],
       ),
     );
   }
 
   Widget _buildTitle(){
+    double paddingHeight=0;
+    if(searchDone==true){
+      switch (foodList.list.length) {
+        case 0:
+          paddingHeight = screenHeight/3.3;
+          break;
+        case 1:
+          paddingHeight = screenHeight/4.2;
+          break;
+        case 2:
+          paddingHeight = screenHeight/4.9;
+          break;
+        case 3:
+          paddingHeight = screenHeight/7.2;
+          break;
+        case 4:
+          paddingHeight = screenHeight/12;
+          break;
+        case 5:
+          paddingHeight = screenHeight/25;
+          break;
+      }
+    }
     return Center(
       child: Container(
-        padding: searchDone==true?EdgeInsets.fromLTRB(22, 25, 0, 5)
-            :EdgeInsets.fromLTRB(22, 0, 0, 30),
+        padding: searchDone==true?EdgeInsets.fromLTRB(22, paddingHeight, 0, 5)
+            :EdgeInsets.fromLTRB(22, screenHeight/3.3, 0, 30),
         alignment: Alignment.centerLeft,
         child: Text(
-          'Food Form',
+          'Enter Food',
           style: TextStyle(
               fontSize: 55,
               fontWeight: FontWeight.bold
@@ -136,54 +235,64 @@ class _FoodFormState extends State<FoodForm> {
 
   @override
   Widget build(BuildContext context) {
+    screenHeight=MediaQuery.of(context).size.height;
+    screenWidth=MediaQuery.of(context).size.width;
+    print('h: $screenHeight x w: $screenWidth');
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildTitle(),
-              _buildName(),
-              SizedBox(height: 10,),
-              isSearching==true?Column(
-                children: [
-                  SizedBox(height: 20),
-                  CircularProgressIndicator(),
-                  SizedBox(height: 20),
-                ],
-              ):SizedBox.shrink(),
-              searchDone==true?_buildList(foodList):SizedBox.shrink(),
-              Builder(builder: (context) => ElevatedButton(
-                child: Text(
-                  'Search',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
-                ),
-                onPressed: () async {
-                  if (!_formKey.currentState.validate()) {
-                    return;
-                  }
-                  setState(() {
-                    isSearching=true;
-                    searchDone=false;
-                    _formKey.currentState.save();
-                  });
-                  foodData = await getData();
-                  if(foodData=='No results'){
-                    foodList=FoodList(list: []);
-                  }else{
-                    foodList=FoodList.fromData(foodData);
-                  }
-                  setState(() {
-                    isSearching=false;
-                    searchDone=true;
-                  });
-                },
-              )),
-              SizedBox(height:90,)
-            ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildTitle(),
+                _buildName(),
+                SizedBox(height: 10,),
+                isSearching==true?Column(
+                  children: [
+                    SizedBox(height: 30),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: CircularProgressIndicator()
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ):SizedBox.shrink(),
+                searchDone==true?_buildList(foodList):SizedBox.shrink(),
+                Builder(builder: (context) => ElevatedButton(
+                  child: Text(
+                    'Search',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+                  ),
+                  onPressed: () async {
+                    if (!_formKey.currentState.validate()) {
+                      return;
+                    }
+                    setState(() {
+                      isSearching=true;
+                      searchDone=false;
+                      _formKey.currentState.save();
+                    });
+                    foodData = await getData();
+                    if(foodData=='No results'){
+                      foodList=FoodList(list: []);
+                    }else{
+                      foodList=FoodList.fromData(foodData);
+                    }
+                    setState(() {
+                      isSearching=false;
+                      searchDone=true;
+                    });
+                  },
+                )),
+                searchDone!=true?SizedBox(height:screenHeight/9,):
+                    SizedBox.shrink(),
+              ],
+            ),
           ),
         ),
       ),
