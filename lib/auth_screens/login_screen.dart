4e229941da05d2c4ce6bd98cc5ef77e0f9c0dec8
NeerajNair <nairnera19ce@student.mes.ac.in>
2021-04-22 11:46:02 +0530

    signup page

--- lib/auth_screens/login_screen.dart
+++ lib/auth_screens/login_screen.dart
@@ -14,94 +14,73 @@ class _LoginScreenState extends State<LoginScreen> {
 
   String error = '';
   bool loading = false;
+  bool errorOccured= true;
 
   // text field state
-  String email = 'aadi1@xyz.com';
-  String password = 'aadi123';
+  String email = '';
+  String password = '';
 
   Widget _buildEmail(){
-    return Card(
-      elevation: 5,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        // side: BorderSide(
-        //   width: 2,
-        //   color: Theme.of(context).disabledColor
-        // ),
-        borderRadius: BorderRadius.circular(15.0),
+    return TextFormField(
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent, width: 2, style: BorderStyle.solid, ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2, style: BorderStyle.solid, ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          filled: true,
+          fillColor: Theme.of(context).colorScheme.surface,
+          labelText: 'Email Id',
+          labelStyle: TextStyle(fontSize: 25),
+          floatingLabelBehavior: FloatingLabelBehavior.never
       ),
-      child: TextFormField(
-        decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent, width: 2, style: BorderStyle.solid, ),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2, style: BorderStyle.solid, ),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            filled: true,
-            fillColor: Theme.of(context).colorScheme.surface,
-            labelText: 'Email Id',
-            labelStyle: TextStyle(fontSize: 25),
-            floatingLabelBehavior: FloatingLabelBehavior.never
-        ),
-        keyboardType: TextInputType.emailAddress,
-        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
-        validator: (String value) {
-          if (value.isEmpty) {
-            return 'Invalid Email Id';
-          }
-          return null;
-        },
-        onSaved: (String value) {
-          email= value;
-        },
-      ),
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Invalid Email Id';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        email= value;
+      },
     );
   }
 
   Widget _buildPassword(){
-    return Card(
-      elevation: 5,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        // side: BorderSide(
-        //   width: 2,
-        //   color: Theme.of(context).disabledColor
-        // ),
-        borderRadius: BorderRadius.circular(15.0),
+    return TextFormField(
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent, width: 2, style: BorderStyle.solid, ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2, style: BorderStyle.solid, ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          filled: true,
+          fillColor: Theme.of(context).colorScheme.surface,
+          labelText: 'Password',
+          labelStyle: TextStyle(fontSize: 25),
+          floatingLabelBehavior: FloatingLabelBehavior.never
       ),
-      child: TextFormField(
-        decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent, width: 2, style: BorderStyle.solid, ),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2, style: BorderStyle.solid, ),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            filled: true,
-            fillColor: Theme.of(context).colorScheme.surface,
-            labelText: 'Password',
-            labelStyle: TextStyle(fontSize: 25),
-            floatingLabelBehavior: FloatingLabelBehavior.never
-        ),
-        obscureText: true,
-        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
-        validator: (String value) {
-          if (value.isEmpty) {
-            return 'Password cannot be empty';
-          }
-          return null;
-        },
-        onSaved: (String value) {
-          password= value;
-        },
-      ),
+      obscureText: true,
+      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Password cannot be empty';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        password= value;
+      },
     );
   }
 
@@ -121,7 +100,7 @@ class _LoginScreenState extends State<LoginScreen> {
               Container(
                 padding: EdgeInsets.all(20),
                 child: Text(
-                  'Login Page',
+                  'Login',
                   style: TextStyle(
                     fontSize: 40,
                   ),
@@ -146,17 +125,28 @@ class _LoginScreenState extends State<LoginScreen> {
                   }
                   setState(() {
                     loading = true;
+                    errorOccured=false;
                     _formKey.currentState.save();
                   });
                   dynamic result = await _auth.signInWithEmailAndPassword(email, password);
                   if(result == null) {
                     setState(() {
                       loading = false;
-                      error = 'Could not sign in with those credentials';
+                      error = 'Could not log in with those credentials';
+                      errorOccured=true;
                     });
                   }
                 },
               ),
+              errorOccured? Text(
+                error,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight:FontWeight.w300,
+                    color: Colors.red,
+                ),
+
+              ): SizedBox.shrink(),
             ],
           ),
         ),
