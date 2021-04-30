c07eb97715a4d77b35346f36fad937d3debd31a5
Pranav <pranav.shegekar@gmail.com>
2021-04-30 17:39:48 +0530

    report_page 2

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -5,6 +5,7 @@ import 'package:dietmate/shared/gradient.dart';
 import 'package:dietmate/shared/loading.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -36,6 +37,103 @@ class _ReportPageState extends State<ReportPage> {
         .get();
     return result;
   }
+  Card _buildLineChart() {
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      elevation: 7,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(54)),
+      color: Colors.grey.shade100.withOpacity(0.1),
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 10,
+            sigmaY: 10,
+          ),
+          child: Container(
+            height: 300 ,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(10),
+            width: double.infinity,
+
+
+            child: LineChart(
+
+              LineChartData(
+                minX:0,
+                maxX:8,
+
+                minY: 0,
+                maxY: 12,
+
+                borderData: FlBorderData(
+                    show: false,
+                ),
+                titlesData: FlTitlesData(
+
+                  leftTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 15,
+                    getTextStyles: (value) => const TextStyle(
+                      color: Colors.white,
+                    )
+                  ),
+                  bottomTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 5,
+                    getTextStyles: (value) => const TextStyle(
+                      color: Colors.white,
+                    )
+                  )
+                ),
+                gridData: FlGridData(
+                  drawHorizontalLine: true,
+                  drawVerticalLine: true,
+                ),
+
+                // backgroundColor: Colors.blueGrey[200],
+
+
+
+                lineBarsData: [
+                  LineChartBarData(
+
+                    spots:[
+                      FlSpot(0, 0),
+                      FlSpot(2, 5),
+                      FlSpot(3, 6),
+                      FlSpot(4, 8),
+                      FlSpot(5, 11),
+                      FlSpot(6, 4),
+                      FlSpot(7, 1),
+
+                    ] ,
+                    isCurved: true,
+                    curveSmoothness: 0.34,
+                    barWidth: 4,
+                    belowBarData: BarAreaData(
+                      show: true,
+                      colors:[
+                        Colors.purpleAccent[100],
+                        Colors.purpleAccent[700],
+                      ]
+                    ),
+                    colors: [
+                      Colors.purpleAccent,
+                    ],
+
+                  ),
+                ],
+
+              ),
+
+              swapAnimationDuration: Duration(seconds: 1),
+              swapAnimationCurve: Curves.linear,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -51,157 +149,123 @@ class _ReportPageState extends State<ReportPage> {
     caloriesGoal=userData.calorieGoal;
 
     return Scaffold(
-        body: FutureBuilder<QuerySnapshot>(
-          future: getData(user),
-          builder: (context, snapshot){
-            if(snapshot.connectionState!=ConnectionState.done){
-              //query in progress
-              return Loading();
-            }
-            if(snapshot.hasError){
-              return Container(
-                child: Text(
-                  'Error occurred',
-                ),
-              );
-            }
-            if(snapshot.hasData){
-              FoodListWeek week = FoodListWeek.fromSnapshot(snapshot.data, start);
-              // if(foodList.list.isEmpty){
-              //   //query successful but is empty
-              //   return Container(
-              //     child: Text(
-              //         'Empty'
-              //     ),
-              //   );
-              // }
-              //TODO main code
-              List<FlSpot>spotList=[];
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
 
-              return Container(
-                padding: EdgeInsets.symmetric(horizontal: 26),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        gradient: customGradient,
-                      ),
+            child: FutureBuilder<QuerySnapshot>(
+              future: getData(user),
+              builder: (context, snapshot){
+                if(snapshot.connectionState!=ConnectionState.done){
+                  //query in progress
+                  return Loading();
+                }
+                if(snapshot.hasError){
+                  return Container(
+                    child: Text(
+                      'Error occurred',
                     ),
-                    Column(
+                  );
+                }
+                if(snapshot.hasData){
+                  FoodListWeek week = FoodListWeek.fromSnapshot(snapshot.data, start);
+                  // if(foodList.list.isEmpty){
+                  //   //query successful but is empty
+                  //   return Container(
+                  //     child: Text(
+                  //         'Empty'
+                  //     ),
+                  //   );
+                  // }
+                  //TODO main code
+                  List<FlSpot>spotList=[];
+
+                  return Container(
+                    padding: EdgeInsets.symmetric(horizontal: 26),
+                    child: Column(
                       children: [
-                        Text(
-                          'Report'
-                        ),
-                        Card(
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          elevation: 7,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(54)),
-                          color: Colors.grey.shade100.withOpacity(0.1),
-                          child: ClipRect(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 10,
-                                sigmaY: 10,
-                              ),
-                              child: Container(
-                                height: 300 ,
-                                padding: EdgeInsets.all(20),
-                                margin: EdgeInsets.all(10),
-                                width: double.infinity,
-
-
-                                child: LineChart(
-
-                                  LineChartData(
-                                    minX:0,
-                                    maxX:8,
-
-                                    minY: 0,
-                                    maxY: 12,
-
-                                    borderData: FlBorderData(
-                                        show: false,
-                                    ),
-                                    titlesData: FlTitlesData(
-
-                                      leftTitles: SideTitles(
-                                        showTitles: true,
-                                        reservedSize: 15,
-                                        getTextStyles: (value) => const TextStyle(
-                                          color: Colors.white,
-                                        )
-                                      ),
-                                      bottomTitles: SideTitles(
-                                        showTitles: true,
-                                        reservedSize: 5,
-                                        getTextStyles: (value) => const TextStyle(
-                                          color: Colors.white,
-                                        )
-                                      )
-                                    ),
-                                    gridData: FlGridData(
-                                      drawHorizontalLine: true,
-                                      drawVerticalLine: true,
-                                    ),
-
-                                    // backgroundColor: Colors.blueGrey[200],
-
-
-
-                                    lineBarsData: [
-                                      LineChartBarData(
-
-                                        spots:[
-                                          FlSpot(0, 0),
-                                          FlSpot(2, 5),
-                                          FlSpot(3, 6),
-                                          FlSpot(4, 8),
-                                          FlSpot(5, 11),
-                                          FlSpot(6, 4),
-                                          FlSpot(7, 1),
-
-                                        ] ,
-                                        isCurved: true,
-                                        curveSmoothness: 0.34,
-                                        barWidth: 4,
-                                        belowBarData: BarAreaData(
-                                          show: true,
-                                          colors:[
-                                            Colors.purpleAccent[100],
-                                            Colors.purpleAccent[700],
-                                          ]
-                                        ),
-                                        colors: [
-                                          Colors.purpleAccent,
-                                        ],
-
-                                      ),
-
-                                    ],
-
-
-                                  ),
-
-                                  swapAnimationDuration: Duration(seconds: 1),
-                                  swapAnimationCurve: Curves.linear,
-                                ),
-                              ),
+                        Container(
+                          padding: EdgeInsets.only(top: 36, left: 38),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Report',
+                            style: TextStyle(
+                              fontSize: 30,
                             ),
                           ),
                         ),
+                        Container(
+                          padding: EdgeInsets.only(left: 38),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'of the past 7 days',
+                            style: TextStyle(
+                              fontSize: 19,
+                            ),
+                          ),
+                        ),
+                        _buildLineChart(),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Card(
+                              margin: EdgeInsets.zero,
+                              child: Container(
+                                width: 144,
+                                height: 212,
+                                // FOR LEGENDS
+
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+
+                            Card(
+                              margin: EdgeInsets.zero,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal:21 ,vertical: 16),
+                                height: 212,
+                                width: 222,
+                                child: PieChart(
+                                  PieChartData(
+                                    sections:[
+                                      PieChartSectionData(
+                                        value: 10,
+                                      ),
+                                      PieChartSectionData(
+                                        value:20,
+                                        color: Colors.blueGrey,
+                                      ),
+                                      PieChartSectionData(
+                                        value:40,
+                                        color: Colors.green,
+                                      )
+                                    ]
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
                       ],
                     ),
-                  ],
-                ),
-              );
-            }
-            return Container(
-              child: Text(
-                  'Something went wrong'
-              ),
-            );
-          },
+                  );
+                }
+                return Container(
+                  child: Text(
+                      'Something went wrong'
+                  ),
+                );
+              },
+            ),
+          ),
         )
     );
   }
+
+
 }
