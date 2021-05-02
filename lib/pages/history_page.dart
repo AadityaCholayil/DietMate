c13806d22bf97a28c014d56ea4c9a168216b0a3d
NupoorDhamanse <dhamansenuha19ce@student.mes.ac.in>
2021-05-02 16:31:55 +0530

    calendar v1

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:glass_kit/glass_kit.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HistoryPage extends StatefulWidget {
   @override
@@ -6,6 +8,9 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +20,98 @@ class _HistoryPageState extends State<HistoryPage> {
             image: AssetImage('assets/mid_${Theme.of(context).brightness==Brightness.light?'light':'dark'}.jpg'),
             fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(50,37,0,16),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'History',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            ),
+            GlassContainer(
+              borderRadius: BorderRadius.all(Radius.circular(34.0)),
+              color: Theme.of(context).cardColor.withOpacity(0.6),
+              borderColor: Theme.of(context).colorScheme.surface.withOpacity(0.0),
+              height: 425,
+              width: 360,
+              //isFrostedGlass: true,
+              //frostedOpacity: 0.05,
+              blur: 15,
+              child: Container(
+                padding: EdgeInsets.all(15),
+                child: TableCalendar(
+                  //calendarController: _controller,
+                  focusedDay: DateTime.now(),
+                  firstDay: DateTime(2000),
+                  lastDay: DateTime(2030),
+                  calendarFormat: format,
+                  onFormatChanged: (CalendarFormat _format) {
+                    setState(() {
+                      format = _format;
+                    });
+                  },
+                  startingDayOfWeek: StartingDayOfWeek.sunday,
+                  daysOfWeekVisible: true,
+
+                  //Day Changed
+                  onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                    setState(() {
+                      selectedDay = selectDay;
+                      focusedDay = focusDay;
+                    });
+                    print(focusedDay);
+                  },
+                  selectedDayPredicate: (DateTime date) {
+                    return isSameDay(selectedDay, date);
+                  },
+                  availableGestures: AvailableGestures.horizontalSwipe,
+                  //headerVisible: false,
+                  calendarStyle: CalendarStyle(
+                    isTodayHighlighted: true,
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.lightGreen[600],
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    selectedTextStyle: TextStyle(color: Colors.white),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.green[700],
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    defaultDecoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    weekendDecoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                   ),
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: true,
+                      titleCentered: true,
+                      formatButtonShowsNext: false,
+                      formatButtonDecoration: BoxDecoration(
+                        color: Colors.lightGreen[600],
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      formatButtonTextStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                 ),
+                ),
+          ],
         ),
       ),
     );
