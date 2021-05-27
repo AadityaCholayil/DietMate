86fea07e86eecd2839119029ffd2fb4a942ef4cf
Pranav <pranav.shegekar@gmail.com>
2021-05-27 22:07:19 +0530

    pie chart animation v3

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'dart:ui';
+import 'package:animations/animations.dart';
 
 class ReportPage extends StatefulWidget {
   @override
